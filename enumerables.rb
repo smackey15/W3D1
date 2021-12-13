8a1cbb24cc971d371ec15ebaf88f47ac1c6b09ff
sean <midruss@yahoo.com>
2021-12-13 16:26:50 -0500

    2nd swap

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,4 @@
-# My Each
+##My Each
 # Extend the Array class to include a method named my_each that takes a block, calls the block on every element of the array, and returns the original array. Do not use Enumerable's each method. I want to be able to write:
 class Array
     def my_each(&prc)
@@ -7,11 +7,8 @@ class Array
             prc.call(self[i])
             i += 1
         end
+        self
     end
-    self
-end
-
-
  # calls my_each twice on the array, printing all the numbers twice.
 return_value = [1, 2, 3].my_each do |num|
 puts num
@@ -23,6 +20,23 @@ end
 #      3
 #      1
 #      2
-#      3
-
+#      3#
 p return_value  # => [1, 2, 3]
+
+#My Select
+#Now extend the Array class to include my_select that takes a block and returns a new array containing only elements that satisfy the block. Use your my_each method!#
+#Example:
+
+   def my_select(&prc)
+       result = []
+       self.my_each do |ele|
+        result << ele if prc.call(ele)
+       end
+       result
+    end
+
+end
+
+p a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
